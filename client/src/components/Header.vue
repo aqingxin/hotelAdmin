ea4a58b4e7f4c95a0522a09b4e60a9f2c159ff51
aqingxin <1607440679@qq.com>
2018-12-11 11:10:12 +0800

    引入less配置

---
+++ client/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-header>
+    <div class="header">
+      <div class="header-left">
+        <img src="../assets/images/logo.png" alt="logo">
+        <i class="el-icon-menu"></i>
+      </div>
+    </div>
+  </el-header>
+</template>
+
+
+<style lang="less" scoped>
+  .header {
+    display: flex;
+    align-items: center;
+    background: pink;
+  }
+  .header-left {
+    min-width: 140px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    img {
+      width: 60px;
+      height: 60px;
+    }
+  }
+</style>
+
