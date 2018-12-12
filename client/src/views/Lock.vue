a1983c6452d8d1607ed8abe22a972e4752dfb75c
aqingxin <1607440679@qq.com>
2018-12-12 17:26:28 +0800

    系统锁定界面

---
+++ client/src/views/Lock.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="lock">
+    <Swiper />
+  </div>
+</template>
+
+
+<script>
+import Swiper from '../components/Swiper'
+export default {
+  components:{
+    Swiper,
+  },
+}
+</script>
+
+
+<style lang="less" scoped>
+  .swiper-item {
+    width: 100%;
+    height: 400px;
+  }
+</style>
