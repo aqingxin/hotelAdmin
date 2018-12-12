5f0bdc6d5c3dc73563c0674fabf0eeaa88558f50
aqingxin <1607440679@qq.com>
2018-12-12 20:28:11 +0800

    摒弃swiper组件，改用div展示

--- client/src/components/Swiper.vue
+++ client/src/components/Swiper.vue
@@ -1,58 +1,43 @@
 <template>
   <div class="show-swiper">
-    <swiper :options="swiperOption" ref="mySwiper">
-      <swiper-slide class="swiper-item">I'm Slide 1</swiper-slide>
-      <swiper-slide class="swiper-item">I'm Slide 2</swiper-slide>
-      <swiper-slide class="swiper-item">I'm Slide 3</swiper-slide>
-      <swiper-slide class="swiper-item">I'm Slide 4</swiper-slide>
-      <swiper-slide class="swiper-item">I'm Slide 5</swiper-slide>
-      <swiper-slide class="swiper-item">I'm Slide 6</swiper-slide>
-      <swiper-slide class="swiper-item">I'm Slide 7</swiper-slide>
-      <div class="swiper-pagination"  slot="pagination"></div>
-      <div class="swiper-button-prev" slot="button-prev"></div>
-      <div class="swiper-button-next" slot="button-next"></div>
-    </swiper>
+    <div class="lock-div">
+      <div>
+        <img src="../assets/images/lock.png" alt="lock">
+        <h1>系统锁定中</h1>
+      </div>
+    </div>
   </div>
 </template>
 
 
 <script>
-import 'swiper/dist/css/swiper.css'
-import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
 export default {
-  data(){
-    return {
-      swiperOption:{    //swiper组件配置
-        notNextTick: true,
-        autoplay:{
-          delay:5000,
-          stopOnLastSlide:false,
-          disableOnInteraction:false
-        },
-        pagination : {
-          el:'.swiper-pagination',
-          type:'bullets',
-          clickable:true
-        },
-        prevButton:'.swiper-button-prev',
-        nextButton:'.swiper-button-next',
-        navigation:{
-          nextEl:'.swiper-button-next',
-          prevEl:'.swiper-button-prev'
-        },
-      }
-    }
-  },
-  components:{
-    swiper,
-    swiperSlide
-  },
+  
 }
 </script>
 
 <style lang="less" scoped>
   .swiper-item {
     width: 100%;
-    height: 400px;
+    height: 500px;
+  }
+  .lock-div {
+    width: 100%;
+    height: 500px;
+    background: #24292E;
+    position: relative;
+    div {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%,-50%);
+      display: flex;
+      align-items: center;
+      color: #fff;
+    }
+    h1 {
+      margin-left: 20px;
+    }
   }
 </style>
