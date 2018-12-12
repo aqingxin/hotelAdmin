7018f333eb1e988f3f69735810bd89eac725c841
aqingxin <1607440679@qq.com>
2018-12-12 17:28:23 +0800

    增加系统锁定路由拦截

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
+import Lock from './views/Lock.vue'
 
 Vue.use(Router)
 
@@ -18,17 +19,33 @@ const routes=[
     path: '/login',
     name: 'Login',
     component: Login
-  }
+  },
+  {
+    path: '/lock',
+    name: 'Lock',
+    component: Lock,
+    meta:{
+      requireAuth: true
+    }
+  },
 ]
 
 const router=new Router({
   routes
 });
-
 router.beforeEach((to,from,next)=>{
   if(to.meta.requireAuth&&window.localStorage.getItem('loginToken')==null){
     next({
       path:'/login'
+    })
+  }else{
+    next();
+  }
+
+  if(to.meta.requireAuth&&window.localStorage.getItem('lock')!==null&&eval(window.localStorage.getItem('lock').toLowerCase())){
+    console.log(2222)
+    next({
+      path:'/lock'
     })
   }else{
     next();
