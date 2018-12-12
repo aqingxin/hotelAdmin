f639bd4e1f1a5747e56297be983389c75ca51648
aqingxin <1607440679@qq.com>
2018-12-12 17:27:10 +0800

    检测有无登录

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -13,7 +13,7 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
 import Header from '@/components/Header.vue';
 import Aside from '@/components/Aside.vue';
 import Main from '@/components/Main.vue';
@@ -24,6 +24,25 @@ export default {
     Header,
     Aside,
     Main
+  },
+  mounted(){
+    this.$http.get('http://10.21.40.155:3000/check',{withCredentials : true}).then(res=>{
+      if(res.data.code===210){
+        this.$message({
+          showClose:true,
+          message:'未登录',
+          type:'error'
+        })
+        window.localStorage.removeItem('loginToken');
+        this.$router.push({path:'/login'})
+      }
+    }).catch(err=>{
+      this.$message({
+        showClose:true,
+        message:'网络请求失败',
+        type:'error'
+      })
+    })
   }
 }
 </script>
