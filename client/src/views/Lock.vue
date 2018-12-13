4ac58d192d547540d01ac0765c7643dc681de219
aqingxin <1607440679@qq.com>
2018-12-13 09:50:21 +0800

    完成系统锁定功能

--- client/src/views/Lock.vue
+++ client/src/views/Lock.vue
@@ -1,23 +1,119 @@
 <template>
   <div class="lock">
-    <Swiper />
+    <div class="lock-div">
+      <div>
+        <img src="../assets/images/lock.png" alt="lock">
+        <h1>系统锁定中</h1>
+      </div>
+    </div>
+    <div class="lock-input">
+      <h1>解锁系统</h1>
+      <el-form :model="lockForm" :rules="lock" ref="lockForm" @submit.native.prevent="fUnlockForm('lockForm')">
+        <el-form-item class="lock-item" prop="lockPassword">
+          <el-input class="unlock-input" type="password" v-model="lockForm.lockPassword" placeholder="请输入解锁密码" />
+        </el-form-item>
+        <el-button type="defalut" class="unlock-button" @click="fUnlockForm('lockForm')">unlock</el-button>
+      </el-form>
+    </div>
   </div>
 </template>
 
 
 <script>
-import Swiper from '../components/Swiper'
+import crypto from 'crypto';
 export default {
-  components:{
-    Swiper,
+  data(){
+    return{
+      lockForm:{
+        lockPassword:''
+      },
+      lock:{
+        lockPassword:[
+          { required: true, message: '请输入解锁密码', trigger: 'blur' }
+        ]
+      }
+    }
   },
+  methods:{
+    fUnlockForm(formName){
+      this.$refs[formName].validate((valid)=>{   //表单验证
+        if(valid){
+          var md5=crypto.createHash('md5');    //对密码进行加密
+          md5.update(this.lockForm.lockPassword);
+          var pass = md5.digest('hex');
+          var params = new URLSearchParams();
+          params.append('lockPassword',pass);
+          this.$http.post('http://10.21.40.155:3000/unlock',params).then(res=>{
+            if(res.data.code===200){
+              this.$message({
+                showClose:true,
+                message:res.data.msg,
+                type:'success'
+              })
+              window.localStorage.setItem('lock',false)   //改变系统锁定状态
+              this.$router.push({path:'/'})
+            }else{
+              this.$message({
+                showClose:true,
+                message:res.data.msg,
+                type:'error'
+              })
+            }
+          }).catch(err=>{ 
+            this.$message({
+                showClose:true,
+                message:'网络请求失败',
+                type:'error'
+              })
+          })
+        }else{
+          this.$message({
+            showClose:true,
+            message:'请正确输入解锁表单',
+            type:'error'
+          })
+        }
+      })
+    }
+  }
 }
 </script>
 
 
 <style lang="less" scoped>
-  .swiper-item {
+  .lock-div {
     width: 100%;
-    height: 400px;
+    height: 500px;
+    background: #24292E;
+    position: relative;
+    div {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%,-50%);
+      display: flex;
+      align-items: center;
+      color: #fff;
+    }
+    h1 {
+      margin-left: 20px;
+    }
+  }
+  .lock-input {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    margin-top: 140px;
+    text-align: center;
+    width: 300px;
+    h1 {
+      color: #24292E;
+    }
+  }
+  .unlock-input {
+    margin-top: 20px;
+  }
+  .unlock-button {
+    width: 100%;
   }
 </style>
